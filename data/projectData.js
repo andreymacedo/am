29a60477f0a646525e513fc4d344af78aa51b849
Andrey <andrey.rm@gmail.com>
2023-08-03 08:21:17 -0300

    coming soon

--- data/projectData.js
+++ data/projectData.js
@@ -8,6 +8,7 @@ export const projects = [
     coverImgSrc: "/img/lg-magenta-cover.png?version=4",
     blocks: [
       { type: "text", content: "The LG Magenta initiative is a comprehensive resource hub for architects and designers. It offers access to the latest architectural trends, expert-led master classes, personalized office services, and exclusive previews of LG product launches. Users can download 3D models and detailed specifications for seamless project integration. The initiative also provides an opportunity to visit LG's exclusive showroom, the LG Business Center, and to showcase their projects across LG's brand channels." },
+      { type: "text", content: "Come back soon for the full project." },
       //{ type: "image", src: "/img/lg-magenta-1.png?version=2" },
       //{ type: "image", src: "/img/lg-magenta-2.png?version=2" },
       //{ type: "image", src: "/img/lg-magenta-3.png?version=2" },
@@ -23,6 +24,8 @@ export const projects = [
     coverImgSrc: "/img/tc-cover.png?version=8",
     blocks: [
       { type: "text", content: "TC is a platform that caters to traders with features like zero brokerage and specialized trading tools. It hosts a diverse team of experts and a community of seasoned investors who share insights and strategies. The platform is designed to optimize your investment experience and maximize profits through shared knowledge and exclusive benefits." },
+      { type: "text", content: "Come back soon for the full project." },
+
       //{ type: "image", src: "/img/tc-1.png?version=8" },
       //{ type: "image", src: "/img/tc-2.png?version=8" },
       //{ type: "image", src: "/img/tc-3.png?version=8" },
